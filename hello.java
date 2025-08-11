4f31b85ad0b10d62cc95f18cc1c126cc70735ef3
Nandha Kumar K <nandhakumar.k7071@gmail.com>
2025-08-12 02:41:15 +0530

    P1 Q3 done

--- hello.java
+++ hello.java
@@ -3,16 +3,22 @@ class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int a= scan.nextInt();
-        int b = scan.nextInt();
-        int c = scan.nextInt();
-        int d = a+b+c;
         
-        if(d==180){
-            System.out.println("Triangle can be formed");
+        if(a>=90){
+            System.out.println("Grade A");
         }
             
+        else if(a>=80 && a<90){
+           System.out.println("Grade B");
+        }
+        else if(a>=60 && a<80){
+           System.out.println("Grade C");
+        }
+        else if(a>=35 && a<60){
+           System.out.println("Grade D");
+        }
         else{
-            System.out.println("Triangle can not be formed");
+            System.out.println("Fail");
         }
     }
 }
