d161245991ffa8a07ad9041ed5a892ffbb264b2e
Nandha Kumar K <nandhakumar.k7071@gmail.com>
2025-08-12 01:52:38 +0530

    Q1 done

---
+++ hello.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a= scan.nextInt();
+        String b = scan.next();
+        char c = scan.next().charAt(0);
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+       
+     
+    }
+}
