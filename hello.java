aaa6d6bcbaa1dd09aa57df486071dd0cf45c627b
Nandha Kumar K <nandhakumar.k7071@gmail.com>
2025-08-13 00:12:08 +0530

    P1 Q3 done

--- hello.java
+++ hello.java
@@ -1,24 +1,13 @@
 import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int a= scan.nextInt();
-        switch(a){
-            case 28:
-                System.out.println("Small");
-                break;
-            case 30:
-                System.out.println("Medium");
-                break;
-            case 38:
-                System.out.println("Large");
-                break;
-            case 42:
-                System.out.println("XLarge");
-                break;
-            default:
-                System.out.println("Invalid");
-                break;
+        int x = scan.nextInt();
+        int y = scan.nextInt();
+
+        for (int i = 0; i < y; i++) {
+            System.out.println(x);
         }
     }
 }
