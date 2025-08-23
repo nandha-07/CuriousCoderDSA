bc0bccafd29a15c5970e4c3c6fa0ac7c2fd40172
Nandha Kumar K <nandhakumar.k7071@gmail.com>
2025-08-24 00:58:06 +0530

    P7 S1  done

--- hello.java
+++ hello.java
@@ -5,7 +5,7 @@ class Main {
         int N = scan.nextInt();
         for(int i = 1; i<=N; i++){
             for(int j=1; j<=N-i+1; j++){
-                System.out.print("*");
+                System.out.print(j);
             }
             System.out.println(" ");
         }
