df966fb3a720de1439363dbdd60ab1efb6ef34b1
Nandha Kumar K <nandhakumar.k7071@gmail.com>
2025-08-25 21:34:14 +0530

    Create P13.java

---
+++ P13.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n= scan.nextInt();
+        int count =1;
+        for(int i=1;i<n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(count);
+                System.out.print(" ");
+                count++;
+            }
+            System.out.println();
+        }    
+    }
+}
